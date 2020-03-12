bc4f0dd97ebb89e81261b2b6fc781dd086684fe4
Jitendra Chahal <33056294+ChahalAWS@users.noreply.github.com>
2020-03-12 12:45:07 +0530

    Update lab.tf

--- linuxvm/lab.tf
+++ linuxvm/lab.tf
@@ -1,72 +1,65 @@
 provider "azurerm" {
-    version = 1.38
-    }
-
-# Create virtual network
-resource "azurerm_virtual_network" "TFNet" {
-    name                = "TFVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "East US"
-    resource_group_name = "terraform-deployment-test"
-
-    tags = {
-        environment = "Terraform VNET"
-    }
+  version         = ">= 2.0"
+  features {}
 }
-# Create subnet
-resource "azurerm_subnet" "tfsubnet" {
-    name                 = "default"
-    resource_group_name = "terraform-deployment-test"
-    virtual_network_name = azurerm_virtual_network.TFNet.name
-    address_prefix       = "10.0.1.0/24"
+variable "prefix" {
+  default = "tfvmex"
 }
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = "West US 2"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.2.0/24"
+}
+
 
 #Deploy Public IP
 resource "azurerm_public_ip" "example" {
   name                = "pubip1"
-  location            = "East US"
-  resource_group_name = "terraform-deployment-test"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Dynamic"
   sku                 = "Basic"
 }
 
-#Create NIC
-resource "azurerm_network_interface" "example" {
-  name                = "terraformnictest"  
-  location            = "East US"
-  resource_group_name = "terraform-deployment-test"
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
-    ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.tfsubnet.id 
-    private_ip_address_allocation  = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+	public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
-#Create Boot Diagnostic Account
-resource "azurerm_storage_account" "sadggfdgdgfg" {
-  name                     = "sadggfdgdgfg" 
-  resource_group_name      = "terraform-deployment-test"
-  location                 = "East US"
-   account_tier            = "Standard"
-   account_replication_type = "LRS"
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
 
-   tags = {
-    environment = "Boot Diagnostic Storage"
-    CreatedBy = "Admin"
-   }
-  }
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
 
-#Create Virtual Machine
-resource "azurerm_virtual_machine" "example" {
-  name                  = "ubuntuvmtest"  
-  location              = "East US"
-  resource_group_name   = "terraform-deployment-test"
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_B1s"
-  delete_os_disk_on_termination = true
-  delete_data_disks_on_termination = true
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -74,27 +67,21 @@ resource "azurerm_virtual_machine" "example" {
     sku       = "16.04-LTS"
     version   = "latest"
   }
-
   storage_os_disk {
-    name              = "osdisk1"
-    disk_size_gb      = "128"
+    name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-
   os_profile {
-    computer_name  = "Enter Server Name"
-    admin_username = "vmadmin"
-    admin_password = "Password12345!"
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
   }
-
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
-boot_diagnostics {
-        enabled     = "true"
-        storage_uri = azurerm_storage_account.sadggfdgdgfg.primary_blob_endpoint
-    }
+  tags = {
+    environment = "staging"
+  }
 }
